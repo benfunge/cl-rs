df62237a5d28fe79aa37671483c36dcc1188d945
Ben S <benfunge@protonmail.com>
2021-09-20 19:39:32 +0000

    Linear Search (2.1-3): correction/rewrite
    As alluded to in the last commit, the implementation of linear search we
    had was weird, in that it didn't return the index of the found element,
    but the element itself. As it turns out, I had a good reason for writing
    it like that: it's the only version of the algorithm that works on
    iterators. However, to keep in the spirit of the book, I think it would
    be wise to limit ourselves to arrays (vectors) and return the found
    index, so this commit updates the algorithm, its documentation, and the
    tests.

--- src/searching/linear_search/mod.rs
+++ src/searching/linear_search/mod.rs
@@ -9,20 +9,18 @@
  Linear search is the obvious solution to the question: How do you find something in an array? The
  answer is, of course, that you simply traverse the array from left to right (or right to left,
  whatever direction you prefer) and check if your value is among the elements encountered on the
- way. We choose here to implement the algorithm on iterators for some added generality, and we also
- allow the item type to be anything that can be compared for equality.
+ way.
 */
 #[cfg(test)]
 mod test;
 
-pub fn linear_search<T, I>(data: I, value: T) -> Option<T>
+pub fn linear_search<T>(data: &Vec<T>, value: T) -> Option<usize>
 where
-    I: Iterator<Item = T>,
     T: PartialEq,
 {
-    for element in data {
-        if element == value {
-            return Some(value);
+    for i in 0..data.len() {
+        if value == data[i] {
+            return Some(i);
         }
     }
 
--- src/searching/linear_search/test.rs
+++ src/searching/linear_search/test.rs
@@ -5,12 +5,12 @@ use crate::testing::vector::{ElementOf, NoElementOf};
 
 #[quickcheck]
 fn finds_members(v: ElementOf<i32>) -> bool {
-    linear_search(v.data.into_iter(), v.element).is_some()
+    linear_search(&v.data, v.element).is_some()
 }
 
 #[quickcheck]
 fn doesnt_find_unmember(v: NoElementOf<i32>) -> bool {
-    linear_search(v.data.into_iter(), v.element).is_none()
+    linear_search(&v.data, v.element).is_none()
 }
 
 // Edge cases
@@ -18,7 +18,7 @@ fn doesnt_find_unmember(v: NoElementOf<i32>) -> bool {
 #[test]
 fn no_data() {
     let nothing: Vec<i32> = Vec::new();
-    if let Some(v) = linear_search(nothing.into_iter(), 0) {
+    if let Some(v) = linear_search(&nothing, 0) {
         assert!(false, "Expected None, got Some({})", v);
     }
 }
@@ -27,11 +27,11 @@ fn no_data() {
 fn only_element() {
     let value = 12;
     let data = vec![value];
-    match linear_search(data.into_iter(), value) {
+    match linear_search(&data, value) {
         Some(v) => assert_eq!(
-            v, value,
+            v, 0,
             "Got value {}, which does not match the search parameter {}!",
-            v, value
+            v, 0
         ),
         None => assert!(false, "Expected value {}, got nothing!", value),
     }
